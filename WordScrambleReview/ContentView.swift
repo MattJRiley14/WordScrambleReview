989604d734a507765a4093be6dc7afce4a319b76
Matt Riley <matthew.riley@barren.kyschools.us>
2024-11-06 04:26:06 -0600

    Initial Commit

---
+++ WordScrambleReview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScrambleReview
+//
+//  Created by BCCS 2022 on 11/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScrambleReview/WordScrambleReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleReviewApp.swift
+//  WordScrambleReview
+//
+//  Created by BCCS 2022 on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
